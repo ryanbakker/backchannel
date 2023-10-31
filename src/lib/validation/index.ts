0263089dce3ed4afe32fab25908aad10b5f27500
ryanbakkerNZ <rmbakker2002@gmail.com>
2023-11-01 02:57:00 +1300

    Create Post & Home Post List

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -17,3 +17,16 @@ export const SigninValidation = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters." }),
 });
+
+export const PostValidation = z.object({
+  caption: z
+    .string()
+    .min(5, { message: "Caption must contain at least 5 characters" })
+    .max(2200, { message: "Caption over 2200 character limit" }),
+  file: z.custom<File[]>(),
+  location: z
+    .string()
+    .min(2, { message: "Location must contain at least 2 character" })
+    .max(100, { message: "Location over 100 character limit" }),
+  tags: z.string(),
+});
