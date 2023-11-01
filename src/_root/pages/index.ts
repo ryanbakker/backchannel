a21cf0c4cb7e35523780107e64431762b750fa0f
ryanbakkerNZ <rmbakker2002@gmail.com>
2023-11-02 02:29:59 +1300

    Removed likes page from index

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -7,4 +7,3 @@ export { default as UpdateProfile } from "./UpdateProfile";
 export { default as EditPost } from "./EditPost";
 export { default as PostDetails } from "./PostDetails";
 export { default as AllUsers } from "./AllUsers";
-export { default as LikedPosts } from "./LikedPosts";
